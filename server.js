dc38186ad8369b6550cc28bdb85b024db8dab7eb
Jeffrey Yang <54513379+jyang19class@users.noreply.github.com>
2021-10-31 03:33:27 -0400

    5

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.listen(HTTP_PORT, () => {
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API is works! (200)"});
+    res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
 
